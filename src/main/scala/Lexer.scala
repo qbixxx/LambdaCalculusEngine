9b70f5fc564ddd5c6b11da3a42a77c9ba3108109
vcalomino <vcalomino@fi.uba.ar>
2024-05-25 18:09:39 -0300

    modificacion lexer

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -1,19 +1,19 @@
 sealed trait Token
-case class TokenLambda() extends Token
-case class TokenDot() extends Token
-case class TokenParIzq() extends Token
-case class TokenParDer() extends Token
-case class TokenEspacio() extends Token
+case object TokenLambda extends Token
+case object TokenPunto extends Token
+case object TokenParIzq extends Token
+case object TokenParDer extends Token
+case object TokenEspacio extends Token
 case class TokenVariable(name: String) extends Token
 
 object Lexer {
   def tokenize(input: String): List[Token] = input.toList match {
     case Nil => List.empty
-    case 'λ' :: tail => TokenLambda() :: tokenize(tail.mkString)
-    case '.' :: tail => TokenDot() :: tokenize(tail.mkString)
-    case '(' :: tail => TokenParIzq() :: tokenize(tail.mkString)
-    case ')' :: tail => TokenParDer() :: tokenize(tail.mkString)
-    case ' ' :: tail => TokenEspacio() :: tokenize(tail.mkString)
+    case 'λ' :: tail => TokenLambda :: tokenize(tail.mkString)
+    case '.' :: tail => TokenPunto :: tokenize(tail.mkString)
+    case '(' :: tail => TokenParIzq :: tokenize(tail.mkString)
+    case ')' :: tail => TokenParDer :: tokenize(tail.mkString)
+    case ' ' :: tail => TokenEspacio :: tokenize(tail.mkString)
     case _ =>
       val (variable, rest) = input.span(c => c != ' ' && c != 'λ' && c != '.' && c != '(' && c != ')')
       TokenVariable(variable) :: tokenize(rest)
