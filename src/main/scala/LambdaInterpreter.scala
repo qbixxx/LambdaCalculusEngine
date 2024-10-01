1fa3c99a2411465fdc664c57657f22553dd6663e
qbixxx <valentinccomp@gmail.com>
2024-09-30 23:05:55 -0300

    translation

---
+++ src/main/scala/LambdaInterpreter.scala
@@ -0,0 +1,45 @@
+
+import scala.io.StdIn.readLine
+
+object LambdaInterpreter {
+  
+  sealed trait Mode
+  
+  case object CallByName extends Mode
+  
+  case object CallByValue extends Mode
+  
+  case object FreeVariables extends Mode
+  
+  var currentMode: Mode = CallByName
+  
+  def main(): Unit = {
+    val input = readLine("("+currentMode+")$ ")
+    input match {
+      case "mode" => println(currentMode)
+      case "exit" => System.exit(0)
+      case "set call-by-name" =>
+        currentMode = CallByName
+      case "set call-by-value" =>
+        currentMode = CallByValue
+      case "set free-variables" => currentMode = FreeVariables
+      case expression =>
+        val tokens = Lexer.tokenize(expression)
+        val ast = Parser.parse(tokens)
+        currentMode match {
+          case CallByName =>
+            val reducedAst = Reducer.callByName(ast)
+            val reducedExp = Parser.buildWithAST(reducedAst)
+            println(reducedExp)
+          case CallByValue =>
+            val reducedAst = Reducer.callByValue(ast)
+            val reducedExp = Parser.buildWithAST(reducedAst)
+            println(reducedExp)
+          case FreeVariables =>
+            val freeVariables = Reducer.freeVariables(ast)
+            println(freeVariables)
+        }
+    }
+    main()
+  }
+}
--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -1,19 +1,19 @@
 sealed trait Token
 case object TokenLambda extends Token
-case object TokenPunto extends Token
-case object TokenParIzq extends Token
-case object TokenParDer extends Token
-case object TokenEspacio extends Token
+case object TokenDot extends Token
+case object TokenLeftParen extends Token
+case object TokenRightParen extends Token
+case object TokenSpace extends Token
 case class TokenVariable(name: String) extends Token
 
 object Lexer {
   def tokenize(input: String): List[Token] = input.toList match {
     case Nil => List.empty
     case ('λ' | '/') :: tail => TokenLambda :: tokenize(tail.mkString)
-    case '.' :: tail => TokenPunto :: tokenize(tail.mkString)
-    case '(' :: tail => TokenParIzq :: tokenize(tail.mkString)
-    case ')' :: tail => TokenParDer :: tokenize(tail.mkString)
-    case ' ' :: tail => TokenEspacio :: tokenize(tail.mkString)
+    case '.' :: tail => TokenDot :: tokenize(tail.mkString)
+    case '(' :: tail => TokenLeftParen :: tokenize(tail.mkString)
+    case ')' :: tail => TokenRightParen :: tokenize(tail.mkString)
+    case ' ' :: tail => TokenSpace :: tokenize(tail.mkString)
     case _ =>
       val (variable, rest) = input.span(c => c != ' ' && c != '/' && c != 'λ' && c != '.' && c != '(' && c != ')')
       TokenVariable(variable) :: tokenize(rest)
